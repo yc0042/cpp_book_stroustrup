8e41b0e74e62a80cd5a63aad60404fb42c8f4c47
Yash Choudhury <yash.choudhury2004@gmail.com>
2022-07-30 14:53:05 -0400

    ch_2 exercises complete

---
+++ ch_2/exercises.cpp
@@ -0,0 +1,11 @@
+// Exercise 1
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, programming!\nHere we go!\n";
+    keep_window_open();
+    return 0;
+}
+
+// Exercise 2-5: skipped
