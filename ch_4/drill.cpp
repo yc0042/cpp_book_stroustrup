2e253a05ed2cb659b4426f7734c75927f5a98d4c
Yash Choudhury <yash.choudhury2004@gmail.com>
2022-07-31 12:23:12 -0400

    ch_4 drills up to 4 complete

--- ch_4/drill.cpp
+++ ch_4/drill.cpp
@@ -1,5 +1,6 @@
 #include "std_lib_facilities.h"
 
+/*drill 1
 int main()
 {
     int val1, val2;
@@ -9,3 +10,58 @@ int main()
     }
     return 0;
 }
+*/
+
+/* Drill 2
+int main()
+{
+    int val1, val2;
+    while(cin >> val1 >>val2) 
+    {
+        cout << "The smaller number is " << min(val1, val2) << endl;
+        cout << "The larger number is " << max(val1, val2) << endl;
+    }
+    return 0;
+}
+*/
+
+/* Drill 3
+
+int main()
+{
+    int val1, val2;
+    while(cin >> val1 >> val2)
+    {
+        if(val1 == val2)
+        {
+            cout << "The two numbers are equal \n";
+        }
+        else
+        {
+            cout << "The two numbers are NOT equal \n";
+        }
+    }
+    return 0;
+}
+*/
+
+/* Drill 4
+int main()
+{
+    double val1, val2;
+    while(cin >> val1 >> val2)
+    {
+        if(val1 == val2)
+        {
+            cout << "The numbers are equal \n";
+        }
+        else
+        {
+            cout << "The numbers are NOT equal \n";
+        }
+    }
+    return 0;
+}
+*/
+
+// Drill 5
